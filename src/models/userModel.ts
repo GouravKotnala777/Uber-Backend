80fde5c627b716fb830fcba2f2b6dc2141052295
gouravkotnala <gouravkotnala777@gmail.com>
2024-11-28 23:11:57 +0530

    refactor(userModel.ts) : set select flag of password field false

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -19,7 +19,8 @@ const userSchema = new mongoose.Schema<UserTypes>({
     },
     password:{
         type:String,
-        required:true
+        required:true,
+        select:false
     },
     mobile:{
         type:String,
