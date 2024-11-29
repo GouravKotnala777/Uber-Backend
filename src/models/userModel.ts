80490ac059be736dabe290f0cde5f5222ac68c58
gouravkotnala <gouravkotnala777@gmail.com>
2024-11-29 19:18:26 +0530

    feat(userModel.ts) : add hashpassword, comparePassword, generateToken, verifyToken methods

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,4 +1,6 @@
 import mongoose, { Model } from "mongoose";
+import bcryptjs from "bcryptjs";
+import jsonWebToken, { JwtPayload } from "jsonwebtoken";
 
 export interface UserTypes {
     name:string;
@@ -6,6 +8,9 @@ export interface UserTypes {
     password:string;
     mobile:string;
     gender:"male"|"female"|"other";
+    comparePassword:(password:string) => Promise<boolean>;
+    generateToken:(userID:string) => Promise<string>;
+    verifyToken:() => JwtPayload;
 }
 
 const userSchema = new mongoose.Schema<UserTypes>({
@@ -31,6 +36,29 @@ const userSchema = new mongoose.Schema<UserTypes>({
         enum:["male", "female", "other"]
     }
 });
+
+userSchema.pre("save", async function(next){
+    if (this.isModified("password")) {
+        const hashedPassword = bcryptjs.hash(this.password, 7);
+        this.password = await hashedPassword;
+    }
+    next();
+});
+
+userSchema.methods.comparePassword = async function(password:string) {
+    const isPasswordMatch = await bcryptjs.compare(password, this.password);
+    return isPasswordMatch;
+};
+
+userSchema.methods.generateToken = async function(userID:string) {
+    const token = await jsonWebToken.sign({_id:userID}, process.env.JWT_SECRET as string, {expiresIn:"3d"});
+    return token;
+};
+userSchema.methods.verifyToken = async function(token:string) {
+    const verifyToken = await jsonWebToken.verify(token, process.env.JWT_SECRET as string);
+    return verifyToken;
+};
+
 
 const userModel:Model<UserTypes> = mongoose.models.UserTypes || mongoose.model<UserTypes>("User", userSchema);
 
