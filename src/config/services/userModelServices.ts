9f88d99a9c53cc0e3b9442d1dddf476f2e0ec50d
gouravkotnala <gouravkotnala777@gmail.com>
2024-11-30 15:38:30 +0530

    feat(userModelServices.ts) : add option for password selection in findUser function

--- src/config/services/userModelServices.ts
+++ src/config/services/userModelServices.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import User from "../../models/userModel.js";
+import { ErrorHandler } from "../../utils/utilityClasses.js";
 
 export const createUser = async({name, email, password, mobile, gender}:{name:string; email:string; password:string; mobile:string; gender:"male"|"female"|"other";}) => {
     if (!name || !email || !password || !mobile || !gender) throw new Error("this is the error...");
@@ -15,12 +16,22 @@ export const findUserByID = async({userID}:{userID:mongoose.Schema.Types.ObjectI
     if (!findUserById)  throw new Error("this is the error...");
     return findUserById;
 }
-export const findUser = async({email, mobile}:{email?:string; mobile?:string;}) => {
-    if (!email && !mobile) throw new Error("this is the error...");
-    const findUser = await User.findOne({
-        ...(email && {email}),
-        ...(mobile && {mobile}),
-    });
+export const findUser = async({email, mobile}:{email?:string; mobile?:string;}, options?:{selectPassword:boolean;}) => {
+    if (!email && !mobile) throw new ErrorHandler("All fields are required", 400);
+    
+    let findUser = null;
+    if (options?.selectPassword) {
+        findUser = await User.findOne({
+            ...(email && {email}),
+            ...(mobile && {mobile}),
+        }).select("+password");
+    }
+    else{
+        findUser = await User.findOne({
+            ...(email && {email}),
+            ...(mobile && {mobile}),
+        });
+    }
     return findUser;
 }
 export const deleteUserByID = async({userID}:{userID:mongoose.Schema.Types.ObjectId;}) => {
