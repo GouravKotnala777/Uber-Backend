c3fdcc7a147871f4c2df6df99b3423d654acd947
gouravkotnala <gouravkotnala777@gmail.com>
2024-12-02 13:11:29 +0530

    feat(driverModel.ts) : add driver schema

---
+++ src/models/driverModel.ts
@@ -0,0 +1,56 @@
+import jsonwebtoken from "jsonwebtoken";
+import mongoose, { Model } from "mongoose";
+
+export interface DriverTypes {
+    _id:mongoose.Schema.Types.ObjectId;
+    userID:mongoose.Schema.Types.ObjectId;
+    licenseNumber:string;
+    vehicleDetailes:{
+        vehicleType:"car"|"motorcycle"|"auto"|"van";
+        vehicleModel:string;
+        vehicleNumber:string;
+        vehicleColor:string;
+    },
+    availabilityStatus:boolean;
+    rating:number;
+    createdAt:Date;
+    updatedAt:Date;
+
+    generateToken:(driverID:mongoose.Schema.Types.ObjectId) => Promise<string>;
+}
+
+const driverSchema = new mongoose.Schema<DriverTypes>({
+    userID:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    licenseNumber:{
+        type:String,
+        required:true
+    },
+    vehicleDetailes:{
+        vehicleType:String,
+        vehicleModel:String,
+        vehicleNumber:String,
+        vehicleColor:String
+    },
+    availabilityStatus:{
+        type:Boolean
+    },
+    rating:{
+        type:Number,
+        default:0
+    }
+}, {
+    timestamps:true
+});
+
+driverSchema.methods.generateToken = async function (driverID:mongoose.Schema.Types.ObjectId) {
+    const driverToken = await jsonwebtoken.sign({_id:driverID}, process.env.JWT_SECRET_DRIVER as string, {expiresIn:"3d"});
+    return driverToken;
+}
+
+const driverModel:Model<DriverTypes> = mongoose.models.Driver || mongoose.model<DriverTypes>("Driver", driverSchema);
+
+export default driverModel;
