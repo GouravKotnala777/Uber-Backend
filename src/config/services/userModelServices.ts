44e9d0294b7d2678e53680dc2fbbb1673b5e5de0
gouravkotnala <gouravkotnala777@gmail.com>
2024-12-10 14:22:30 +0530

    refactor(userModelServices.ts) : add error handling messages

--- src/config/services/userModelServices.ts
+++ src/config/services/userModelServices.ts
@@ -2,20 +2,23 @@ import mongoose from "mongoose";
 import User from "../../models/userModel.js";
 import { ErrorHandler } from "../../utils/utilityClasses.js";
 
+// Create new user
 export const createUser = async({name, email, password, mobile, gender}:{name:string; email:string; password:string; mobile:string; gender:"male"|"female"|"other";}) => {
-    if (!name || !email || !password || !mobile || !gender) throw new Error("this is the error...");
+    if (!name || !email || !password || !mobile || !gender) throw new ErrorHandler("All fields are required", 400);
     const newUser = await User.create({
         name, email, password, mobile, gender
     });
-    if (!newUser)  throw new Error("this is the error...");
+    if (!newUser)  throw new ErrorHandler("Internal server error", 500);
     return newUser;
-}
+};
+// Find user by _id
 export const findUserByID = async({userID}:{userID:mongoose.Schema.Types.ObjectId;}) => {
-    if (!userID) throw new Error("this is the error...");
+    if (!userID) throw new ErrorHandler("UserID not found", 404);
     const findUserById = await User.findById(userID);
-    if (!findUserById)  throw new Error("this is the error...");
+    if (!findUserById)  throw new ErrorHandler("User not found", 404);
     return findUserById;
-}
+};
+// Find user by email or mobile
 export const findUser = async({email, mobile}:{email?:string; mobile?:string;}, options?:{selectPassword:boolean;}) => {
     if (!email && !mobile) throw new ErrorHandler("All fields are required", 400);
     
@@ -33,10 +36,11 @@ export const findUser = async({email, mobile}:{email?:string; mobile?:string;}, 
         });
     }
     return findUser;
-}
+};
+// Delete user by _id
 export const deleteUserByID = async({userID}:{userID:mongoose.Schema.Types.ObjectId;}) => {
-    if (!userID) throw new Error("this is the error...");
+    if (!userID) throw new ErrorHandler("UserID not found", 404);
     const deleteUserById = await User.findByIdAndDelete(userID);
-    if (!deleteUserById)  throw new Error("this is the error...");
+    if (!deleteUserById)  throw new ErrorHandler("Internal server error", 500);
     return deleteUserById;
-}
+};
