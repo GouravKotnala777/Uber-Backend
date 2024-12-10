be27d16aa2ad7ffd16709a8f78d4b96b0328db53
gouravkotnala <gouravkotnala777@gmail.com>
2024-12-10 14:23:42 +0530

    refactor(userController.ts) : take user name value from firstName and lastName input field

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -7,13 +7,13 @@ import { cookieOptions } from "../utils/constants.js";
 // User register
 export const register = async(req:Request, res:Response, next:NextFunction) => {
     try {
-        const {name, email, password, mobile, gender}:{name:string; email:string; password:string; mobile:string; gender:"male"|"female"|"other";} = req.body;
-
+        const {firstName, lastName, email, password, mobile, gender}:{firstName:string; lastName:string; email:string; password:string; mobile:string; gender:"male"|"female"|"other";} = req.body;
+        
         const isUserExists = await findUser({email});
 
         if (isUserExists) return next(new ErrorHandler("Email already exist", 301));
 
-        const createNewUser = await createUser({name, email, password, mobile, gender});
+        const createNewUser = await createUser({name:firstName.concat(lastName), email, password, mobile, gender});
 
         res.status(200).json({success:true, message:"register successful", jsonData:createNewUser})
     } catch (error) {
