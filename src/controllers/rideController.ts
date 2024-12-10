59adcb395324afab7930c066cda6ce8788c23c66
gouravkotnala <gouravkotnala777@gmail.com>
2024-12-10 14:12:16 +0530

    feat(rideController.ts) : add getOTP function

--- src/controllers/rideController.ts
+++ src/controllers/rideController.ts
@@ -1,31 +1,39 @@
 import { NextFunction, Request, Response } from "express";
 import mongoose from "mongoose";
 import Ride, { RideStatusTypes } from "../models/rideModel.js";
-import { findByIdAndUpdateRide } from "../config/services/rideModelServices.js";
+import { createRide, findByIdAndUpdateRide } from "../config/services/rideModelServices.js";
 import { ErrorHandler } from "../utils/utilityClasses.js";
+import { VehicleTypeTypes } from "../models/driverModel.js";
+import crypto from "crypto";
 
 // Create new ride request
 export const createRideRequest = async(req:Request, res:Response, next:NextFunction) => {
     try {
         const {
-            driverID, passengerID, distance, pickupLocation, dropoffLocation, status
+            passengerID, pickupLocation, dropoffLocation, vehicleType
         }:{
-            driverID:mongoose.Schema.Types.ObjectId;
             passengerID:mongoose.Schema.Types.ObjectId;
-            distance:string;
             pickupLocation:string;
             dropoffLocation:string;
-            status:RideStatusTypes;
+            vehicleType:VehicleTypeTypes;
         } = req.body;
 
-        const newRide = await Ride.create({
-            driverID, passengerID, distance, pickupLocation, dropoffLocation, status
+        const newRide = await createRide({
+            passengerID, pickupLocation, dropoffLocation, vehicleType
         });
 
         res.status(200).json({success:true, message:"Requested for cab", jsonData:newRide});
     } catch (error) {
         next(error);
     }
+};
+// Gernerate an OTP
+export function getOTP(num:number){
+    function generateOTP(num:number){
+        const otp = crypto.randomInt(Math.pow(10, num-1), Math.pow(10, num)).toString();
+        return otp;
+    };
+    return generateOTP(num);
 };
 // Accept ride request
 export const acceptRideRequest = async(req:Request, res:Response, next:NextFunction) => {
