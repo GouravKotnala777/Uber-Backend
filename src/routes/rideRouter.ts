9099b685f8796f02ce209b27f32d71b3c763081e
gouravkotnala <gouravkotnala777@gmail.com>
2024-12-13 22:44:00 +0530

    feat(rideRouter.ts) : add route for getFareOfTrip controller

--- src/routes/rideRouter.ts
+++ src/routes/rideRouter.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { isUserAuthenticated } from "../middlewares/auth.js";
-import { acceptRideRequest, createRideRequest } from "../controllers/rideController.js";
+import { acceptRideRequest, createRideRequest, getFareOfTrip } from "../controllers/rideController.js";
 
 const rideRouter = express.Router();
 
 rideRouter.route("/create").post(isUserAuthenticated, createRideRequest);
 rideRouter.route("/accept").post(isUserAuthenticated, acceptRideRequest);
+rideRouter.route("/get-fare").post(isUserAuthenticated, getFareOfTrip);
 
 export default rideRouter;
