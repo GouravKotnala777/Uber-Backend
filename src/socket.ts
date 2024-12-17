d42357a3e684a8d19f98c1f9c3436b0f259e626c
gouravkotnala <gouravkotnala777@gmail.com>
2024-12-17 22:27:10 +0530

    feat(socket.ts) : add some socket events

--- src/socket.ts
+++ src/socket.ts
@@ -2,6 +2,7 @@ import { IncomingMessage, Server as HTTPSERVER, ServerResponse } from "http";
 import {DefaultEventsMap, Server} from "socket.io";
 import User from "./models/userModel.js";
 import Driver from "./models/driverModel.js";
+import { ErrorHandler } from "./utils/utilityClasses.js";
 
 let io:Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>|null;
 
@@ -17,9 +18,7 @@ export const initializeSocket = (server:HTTPSERVER<typeof IncomingMessage, typeo
     io.on("connection", (socket) => {
         console.log(`client connected ${socket.id}`);
 
-        socket.on("join", async(data:{userID:string; userType:"user"|"driver"|"admin";}) => {
-            const {userID, userType} = data;
-
+        socket.on("join", async({userID, userType}:{userID:string; userType:"user"|"driver"|"admin";}) => {            
             console.log(`${userID} joined as ${userType}`);
             
             if (userType === "user") {
@@ -28,24 +27,44 @@ export const initializeSocket = (server:HTTPSERVER<typeof IncomingMessage, typeo
                 });
             }
             else if (userType === "driver") {
-                await Driver.findByIdAndUpdate(userID, {
+                await User.findByIdAndUpdate(userID, {
                     socketID:socket.id
                 });
             }
         });
 
+        socket.on("update-driver-location", async(data:{
+            message:{
+                passengerSocketID:string;
+                driverID:string;
+                eventName:string;
+                location:{ltd:number; lng:number;}};
+        }) => {
+            console.log("FFFFFFFFFFFFFFFFFFFFFFFF");
+            console.log(data);
+            
+            const {driverID, location, eventName, passengerSocketID} = data.message;
+            if (!location || !location.ltd || !location.lng) return socket.emit("error", {message:"Invalid location data"});
+
+            
+            // ---------------------------------------------------------------await Driver.findByIdAndUpdate(driverID, {
+            // --------------------------------------------------------------------------------   location
+            //});
+            io?.to(passengerSocketID).emit(eventName, {driverID, location});
+        });
+
         socket.on("disconnect", () => {
             console.log(`client disconnected ${socket.id}`);
         });
-    })
+    });
 };
 
-export const sendMessageToSocketId = ({socketID, message}:{socketID:string; message:string;}) => {
+export const sendMessageToSocketId = ({socketID, eventName, message}:{socketID:string; eventName:string; message:string|Record<string, unknown>;}) => {
     if (io) {
-        io.to(socketID).emit("message", message)
+        //console.log({socketID, eventName, message});
+        io.to(socketID).emit(eventName, message);
     }
     else{
         console.log(`socket.io not initialized`);
-        
     }
 };
