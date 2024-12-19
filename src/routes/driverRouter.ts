77c23678ec6e1603d16611466df1c887989eac20
gouravkotnala <gouravkotnala777@gmail.com>
2024-12-19 15:46:05 +0530

    test(driverRouter.ts) : remove nearbyDrivers routing for development

--- src/routes/driverRouter.ts
+++ src/routes/driverRouter.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import { isUserAuthenticated } from "../middlewares/auth.js";
-import { allNearbyDrivers, driverLogin, driverProfile, driverRegister } from "../controllers/driverController.js";
+import { driverLogin, driverProfile, driverRegister } from "../controllers/driverController.js";
+//import { allNearbyDrivers, driverLogin, driverProfile, driverRegister } from "../controllers/driverController.js";
 
 const driverRouter = express.Router();
 
 driverRouter.route("/register").post(isUserAuthenticated, driverRegister);
 driverRouter.route("/login").post(isUserAuthenticated, driverLogin);
 driverRouter.route("/me").get(isUserAuthenticated, driverProfile);
-driverRouter.route("/nearbyDrivers").get(isUserAuthenticated, allNearbyDrivers);
+//driverRouter.route("/nearbyDrivers").get(isUserAuthenticated, allNearbyDrivers);
 
 export default driverRouter;
