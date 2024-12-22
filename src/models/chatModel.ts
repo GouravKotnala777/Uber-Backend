34246468e52fe2acbdcdab24e09ffa128c5de74d
gouravkotnala <gouravkotnala777@gmail.com>
2024-12-22 13:58:33 +0530

    feat(chatModel.ts) : add chat schema model

---
+++ src/models/chatModel.ts
@@ -0,0 +1,30 @@
+import mongoose, { Model } from "mongoose";
+
+export interface ChatTypes{
+    sender:mongoose.Schema.Types.ObjectId;
+    receiver:mongoose.Schema.Types.ObjectId;
+    content:string;
+    createdAt:Date;
+}
+
+const chatSchema = new mongoose.Schema<ChatTypes>({
+    sender:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    receiver:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    content:{
+        type:String,
+        required:true,
+        maxlength:[20, "Chat content length should be less than 100 words"]
+    }
+}, {timestamps:true});
+
+const chatModel:Model<ChatTypes> = mongoose.models.Chats || mongoose.model<ChatTypes>("Chat", chatSchema);
+
+export default chatModel;
