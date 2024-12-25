a6173c9b40188611dfd13e818ff3738c1be59e90
gouravkotnala <gouravkotnala777@gmail.com>
2024-12-25 19:14:12 +0530

    test(app.ts) : change origin array of cors

--- src/app.ts
+++ src/app.ts
@@ -27,11 +27,7 @@ const PORT = process.env.PORT || 8000;
 connectDB();
 
 app.use(cors({
-    origin:[
-        process.env.CLIENT_URL as string,
-        process.env.CLIENT_URL2 as string,
-        process.env.CLIENT_URL3 as string,
-    ],
+    origin:[process.env.CLIENT_URL as string],
     methods:["GET", "POST"],
     credentials:true
 }));
