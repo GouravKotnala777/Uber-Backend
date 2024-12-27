e307f2d5814a1023befee4ebbfa4e8cfbede4fec
gouravkotnala <gouravkotnala777@gmail.com>
2024-12-27 13:28:29 +0530

    feat(userController.ts & userRouter.ts) : add a controller for update my profile named 'updateMyProfile'

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { createUser, findUser } from "../config/services/userModelServices.js";
+import { createUser, findUser, findUserByID, findUserByIDAndUpdate } from "../config/services/userModelServices.js";
 import { ErrorHandler } from "../utils/utilityClasses.js";
 import { AuthenticatedRequest } from "../middlewares/auth.js";
 import { cookieOptions } from "../utils/constants.js";
@@ -53,6 +53,34 @@ export const myProfile = async(req:Request, res:Response, next:NextFunction) => 
         const user = (req as AuthenticatedRequest).user;
 
         res.status(200).json({success:true, message:"My profile", jsonData:user});
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+};
+// Update my profile
+export const updateMyProfile = async(req:Request, res:Response, next:NextFunction) => {
+    try {
+        const {name, password, mobile, gender, oldPassword}:{name?:string; password?:string; mobile?:string; gender?:"male"|"female"|"other"; oldPassword:string;} = req.body;
+        const user = (req as AuthenticatedRequest).user;
+
+        const findUserByField = await findUserByID({userID:user._id}, {selectPassword:true});
+
+        if (!oldPassword) return next(new ErrorHandler("Old password not found", 404));
+        
+        const isPasswordMatched = await findUserByField?.comparePassword(oldPassword);
+        
+        if (!isPasswordMatched) return next(new ErrorHandler("Old password not is incorrect", 401));
+
+        const findUserAndUpdate = await findUserByIDAndUpdate({
+            userID:user._id,
+            name,
+            password,
+            mobile,
+            gender
+        });
+
+        res.status(200).json({success:true, message:"Your profile updated", jsonData:findUserAndUpdate});
     } catch (error) {
         console.log(error);
         next(error);
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, myProfile, register } from "../controllers/userController.js";
+import { login, myProfile, register, updateMyProfile } from "../controllers/userController.js";
 import { isUserAuthenticated } from "../middlewares/auth.js";
 
 const userRouter = express.Router();
@@ -7,5 +7,6 @@ const userRouter = express.Router();
 userRouter.route("/register").post(register);
 userRouter.route("/login").post(login);
 userRouter.route("/me").get(isUserAuthenticated, myProfile);
+userRouter.route("/update").post(isUserAuthenticated, updateMyProfile);
 
 export default userRouter;
