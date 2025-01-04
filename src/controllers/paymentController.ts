4fbca97666ddd49e19f1b3bb6edf40ab2a3b74e8
gouravkotnala <gouravkotnala777@gmail.com>
2025-01-04 15:34:05 +0530

    feat(paymentController.ts) : update ride after payment

--- src/controllers/paymentController.ts
+++ src/controllers/paymentController.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import Payment, { PaymentMethodTypes, PaymentStatusTypes } from "../models/paymentModel.js";
 import mongoose from "mongoose";
 import { ErrorHandler } from "../utils/utilityClasses.js";
+import Ride from "../models/rideModel.js";
 
 
 export const createPayment = async(req:Request, res:Response, next:NextFunction) => {
@@ -12,9 +13,14 @@ export const createPayment = async(req:Request, res:Response, next:NextFunction)
 
         if (!rideID || !amount || !paymentMethod || !paymentStatus) return next(new ErrorHandler("All fields are required", 400));
 
+        
         const newPayment = await Payment.create({
             rideID, amount, paymentMethod, paymentStatus
         });
+        
+        const updatedRide = await Ride.findByIdAndUpdate(rideID, {
+            paymentID:newPayment._id
+        }, {new:true});
 
         res.status(200).json({success:true, message:"Payment done", jsonData:newPayment});
     } catch (error) {
