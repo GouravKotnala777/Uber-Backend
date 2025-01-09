a53b1deec251b10707f41fe25338e191e7b9d349
gouravkotnala <gouravkotnala777@gmail.com>
2025-01-09 13:38:34 +0530

    feat(errorMiddleware.ts) : handle mongoose 'CastError' and 'ValidationError' properly

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -5,13 +5,21 @@ import { ErrorHandler } from "../utils/utilityClasses.js";
 export const errorMiddleware = async(err:ErrorHandler, req:Request, res:Response, next:NextFunction) => {
     let message:string = "";
     if (err.name === "CastError") {
-        message = "Wrong ObjectId Format"
+        err.statusCode = 400;
+        message = "Wrong ObjectId Format";
+    }
+    else if (err.name === "ValidationError") {
+        err.statusCode = 400;
+        message = err.message;
     }
     else{
+        err.statusCode = 500;
         message = err.message;
     }
     console.log("%%%%%%%%%%%%%%%%%%%%%%");
-    console.log(err.message);
+    console.log({name:err.name});
+    console.log({message:err.message});
+    console.log({status:err.statusCode});
     console.log("%%%%%%%%%%%%%%%%%%%%%%");
     
     res.status(err.statusCode).json({success:false, message, jsonData:{errName:err.name, errorMessage:err.message}});
