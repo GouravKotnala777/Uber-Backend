6e57bdb82c6ae896a35c932fa642a4e0310fe848
gouravkotnala <gouravkotnala777@gmail.com>
2025-01-10 20:29:36 +0530

    feat(utilityClasses) : add 'sendToken' helper function to create and store token in cookie

--- src/utils/utilityClasses.ts
+++ src/utils/utilityClasses.ts
@@ -1,3 +1,8 @@
+import { NextFunction, Response } from "express";
+import mongoose, { Document } from "mongoose";
+import { UserTypes } from "./types.js";
+import { ObjectId } from "mongoose";
+import { cookieOptions } from "./constants.js";
 
 
 export class ErrorHandler extends Error {
@@ -5,4 +10,10 @@ export class ErrorHandler extends Error {
         super(message);
         this.statusCode = statusCode;
     }
+};
+
+export type GenerateTokenType = {generateToken:(userID:mongoose.Schema.Types.ObjectId) => Promise<string>};
+export const sendToken = async<T extends GenerateTokenType>(res:Response, documentModel:(Document<unknown, {}, T&GenerateTokenType> & (T&GenerateTokenType) & Required<{_id:ObjectId}>), cookieName:string) => {
+    const createToken = await documentModel.generateToken(documentModel._id);
+    res.cookie(cookieName, createToken, cookieOptions);
 };
